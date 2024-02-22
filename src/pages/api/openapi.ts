30b9caa87dc7c7cfb77e23915867abdfe6b5bea4
skrulling <mikkeleb@gmail.com>
2024-02-22 20:06:16 +0100

    catch error

--- src/pages/api/openapi.ts
+++ src/pages/api/openapi.ts
@@ -3,51 +3,63 @@ import path from 'path';
 import type { APIRoute } from "astro";
 import OpenAI from "openai";
 import { schema, systemMessage } from "../../utils/strings";
+import { error } from 'node_modules/astro/dist/core/logger/core';
 
 
 export const POST: APIRoute = async ({ request }) => {
 
   // return await debugMode();
 
-  const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
-  const data = await request.formData();
-  const distance = data.get("distance");
-  const level = data.get("level");
-  const duration = data.get("duration");
+  try {
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: `${systemMessage} ${schema}` },
-      { role: "user", content: `target distance: ${distance}. My running level:${level}. The training plan duration should be: ${duration}`}
-    ],
-    // model: "gpt-4-turbo-preview",
-    model: "gpt-3.5-turbo",
-    response_format: { type: "json_object" }
-  });
+    const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
+    const data = await request.formData();
+    const distance = data.get("distance");
+    const level = data.get("level");
+    const duration = data.get("duration");
 
-  console.log(completion)
-  const answer = completion.choices[0];
-  // // Define the directory where the file will be saved
-  // const dir = './responses';
-  // // Define the full path for the file
-  // const filePath = path.join(dir, 'openai_response.json');
-  // // Save the response to a file for debugging
-  // try {
-  //   // Ensure the directory exists
-  //   await fs.mkdir(dir, { recursive: true });
-  //   // Write the file
-  //   await fs.writeFile(filePath, JSON.stringify({ message: answer }), 'utf8');
-  //   console.log(`Response saved to ${filePath}`);
-  // } catch (error) {
-  //   console.error('Error saving the response:', error);
-  //   // Return an error response or handle it appropriately
-  // }
-  return new Response(
-    JSON.stringify({
-      message: answer
-    }),
-    { status: 200 }
-  );
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: `${systemMessage} ${schema}` },
+        { role: "user", content: `target distance: ${distance}. My running level:${level}. The training plan duration should be: ${duration}` }
+      ],
+      // model: "gpt-4-turbo-preview",
+      model: "gpt-3.5-turbo",
+      response_format: { type: "json_object" }
+    });
+
+    console.log(completion)
+    const answer = completion.choices[0];
+    // // Define the directory where the file will be saved
+    // const dir = './responses';
+    // // Define the full path for the file
+    // const filePath = path.join(dir, 'openai_response.json');
+    // // Save the response to a file for debugging
+    // try {
+    //   // Ensure the directory exists
+    //   await fs.mkdir(dir, { recursive: true });
+    //   // Write the file
+    //   await fs.writeFile(filePath, JSON.stringify({ message: answer }), 'utf8');
+    //   console.log(`Response saved to ${filePath}`);
+    // } catch (error) {
+    //   console.error('Error saving the response:', error);
+    //   // Return an error response or handle it appropriately
+    // }
+    return new Response(
+      JSON.stringify({
+        message: answer
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+
+    return new Response(
+      JSON.stringify({
+        message: error
+      }),
+      { status: 500 }
+    );
+  }
   // Do something with the data, then return a success response
 };
 
